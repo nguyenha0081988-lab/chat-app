e5ae798154758b8abb5d6a21b9863f3aebb21aa9
nguyenha0081988-lab <nguyenha0081988@gmail.com>
2025-10-06 22:05:16 +0700

    server.py

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 # server.py
 
+# Sửa lỗi cho eventlet: Các dòng này PHẢI nằm ở trên cùng
+import eventlet
+eventlet.monkey_patch()
+
+# Các thư viện khác
 import os
 import click
 from flask import Flask, request, jsonify, send_from_directory
@@ -17,7 +22,8 @@ UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a-very-secret-and-strong-key-you-should-change'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -108,8 +114,6 @@ def get_online_users():
             users_info.append({'id': user.id, 'username': user.username})
     return jsonify({'users': users_info})
 
-# ... (Bạn có thể thêm lại các API quản lý file và admin ở đây nếu cần) ...
-
 # --- CÁC SỰ KIỆN SOCKET.IO CHO CHAT ---
 @socketio.on('connect')
 def handle_connect():
@@ -149,5 +153,4 @@ app.cli.add_command(make_admin)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    # Khi chạy local, dùng lệnh này
     socketio.run(app, debug=True, port=5000)
